88a4839bf72ebf94bee78ff0c6cbcbb3fda160e0
Anubhav <kshtriyaanubhav9120@gmail.com>
2026-08-26 21:49:12 +0530

    fix(android): resolve package conflict, enforce unique applicationId, and bump version code

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.nearo"
+    namespace = "com.asiverticals.nearo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,18 +16,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.nearo"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.asiverticals.nearo"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -36,6 +30,9 @@ android {
             signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             isShrinkResources = false
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
--- mobile/android/app/src/main/kotlin/com/asiverticals/nearo/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/asiverticals/nearo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nearo
+package com.asiverticals.nearo
 
 import io.flutter.embedding.android.FlutterActivity
 
