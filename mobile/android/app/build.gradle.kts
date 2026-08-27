8aa1b52fcec94c86b8cd58393143769d831d7c83
Anubhav <kshtriyaanubhav9120@gmail.com>
2026-08-27 09:25:56 +0530

    chore(release): reset app version to 1.0.0 for initial official release

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.nearo.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
-        versionName = "1.0.8"
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
